bb733b37ebdbcbb4725eab068d410eb6c82a0766
saulwade <saulwade29@gmail.com>
2023-05-10 13:21:31 -0600

    added more code

---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/saulwade29" target="_blank" rel="noopener noreferrer">GitHub</a>
+    </footer>
+  );
+};
+
+export default Footer;
