d629fe37d4556b13522ef8dbdd58a1c3a9d582ac
saulwade <saulwade29@gmail.com>
2023-05-10 14:00:41 -0600

    fixed some bugs

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/header';
 import Navigation from './components/navigation';
 import AboutMe from './components/aboutme';
@@ -8,23 +8,20 @@ import Resume from './components/resume';
 import Footer from './components/footer';
 
 function App() {
-    return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Navigation />
-          <main>
-            <Switch>
-              <Route exact path="/" component={aboutme} />
-              <Route path="/portfolio" component={portfolio} />
-              <Route path="/contact" component={contact} />
-              <Route path="/resume" component={resume} />
-            </Switch>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    );
-  }
-  
-  export default App;
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<AboutMe />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app.css'; // If you have an index.css file, otherwise ignore this line
+import App from './app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
